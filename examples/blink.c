64664b8fe240cb0ac3b8a7c24428cd85b0f37b81
Marián <Matiasus@users.noreply.github.com>
2020-02-26 22:28:07 +0100

    Update blink.c

--- examples/blink.c
+++ examples/blink.c
@@ -24,58 +24,92 @@
  * @param   uint32_t
  * @return  void
  */
-void mDelay(uint32_t timelapse)
-{
-  while (timelapse) {
-    // decrement time
-    timelapse--;
-  }
-}
+void mDelay(uint32_t timelapse);
  
 /**
- * @desc    Ld blinking on GPIOA Pin 2
+ * @desc    Led blinking on GPIOA Pin 2
  *
  * @param   void
  * @return  void
  */
-void LedBlink_GPIOA_Pin2() {
-  
-    uint32_t cycles = 100000;
- 
-    // enable clock for corresponding GPIO
-    RCC->APB2ENR |= (1 << RCC_APB2Periph_GPIOA);
-   
-    // ----------------------------
-    // CNF[1:0] = 00 -> Push pull  / GENERAL PURPOSE OUTPUT - MODE[1:0] = 01; 10; 11
-    // CNF[1:0] = 01 -> Open drain / GENERAL PURPOSE OUTPUT - MODE[1:0] = 01; 10; 11
-    // CNF[1:0] = 10 -> Push pull  / ALTERNATE FUNCTION OUTPUT - MODE[1:0] = 01; 10; 11
-    // CNF[1:0] = 01 -> Open drain / ALTERNATE FUNCTION OUTPUT - MODE[1:0] = 01; 10; 11
-    // CNF[1:0] = 00 -> Analog     / INPUT - MODE[1:0] = 00
-    // CNF[1:0] = 01 -> Floating   / INPUT - MODE[1:0] = 00
-    // CNF[1:0] = 10 -> Pull down  / INPUT - MODE[1:0] = 00
-    // CNF[1:0] = 11 -> Pull up    / INPUT - MODE[1:0] = 00
-    // ----------------------------
-    //
-    // GPIOA.Pin_2 - Output push pull
-    GPIOA->CRL = ~((1 << GPIO_CRL_CNF2_0) | (1 << GPIO_CRL_CNF2_1));
-    
-    // MODE[1:0] = 01 // 10 MHz
-    // MODE[1:0] = 10 //  2 MHz
-    // MODE[1:0] = 11 // 50 MHz
-    // ----------------------------
-    // 
-    // speed - 2MHz
-    GPIOA->CRL |= (1 << GPIO_CRL_MODE2_1);
+void LedBlink_GPIOA_Pin2();
 
-    // loop for led blinking
-    while (1) {
-      // led ON
-      GPIOA->BSRR = (1 << GPIO_BSRR_BS2);
-      // delay
-      mDelay(cycles);
-      // led OFF
-      GPIOA->BSRR = (1 << GPIO_BSRR_BR2);
-      // delay
-      mDelay(cycles);      
-    }
+/**
+ * @desc    main
+ *
+ * @param   void
+ * @return  void
+ */
+void main(void)
+{
+  // call led  blink on GPIOA Pin 2
+  LedBlink_GPIOA_Pin2();
 }
+
+/**
+ * @desc    Delay
+ *
+ * @param   void
+ * @return  void
+ */
+void wait(void)
+{
+  // must be defined
+}
+ 
+/**
+ * @desc    Led blinking on GPIOA Pin 2
+ *
+ * @param   void
+ * @return  void
+ */
+void LedBlink_GPIOA_Pin2() 
+{
+  // cycles  
+  uint32_t cycles = 200000;
+
+  // enable clock for corresponding GPIO
+  RCC->APB2ENR |= RCC_APB2Periph_GPIOA;
+ 
+  // ----------------------------
+  // CNF[1:0] = 00 -> Push pull  / GENERAL PURPOSE OUTPUT - MODE[1:0] = 01; 10; 11
+  // CNF[1:0] = 01 -> Open drain / GENERAL PURPOSE OUTPUT - MODE[1:0] = 01; 10; 11
+  // CNF[1:0] = 10 -> Push pull  / ALTERNATE FUNCTION OUTPUT - MODE[1:0] = 01; 10; 11
+  // CNF[1:0] = 01 -> Open drain / ALTERNATE FUNCTION OUTPUT - MODE[1:0] = 01; 10; 11
+  // CNF[1:0] = 00 -> Analog     / INPUT - MODE[1:0] = 00
+  // CNF[1:0] = 01 -> Floating   / INPUT - MODE[1:0] = 00
+  // CNF[1:0] = 10 -> Pull down  / INPUT - MODE[1:0] = 00
+  // CNF[1:0] = 11 -> Pull up    / INPUT - MODE[1:0] = 00
+  // ----------------------------
+  //
+  // GPIOA.Pin_2 - Output push pull
+  GPIOA->CRL = ~(GPIO_CRL_CNF2);
+  
+  // MODE[1:0] = 01 // 10 MHz
+  // MODE[1:0] = 10 //  2 MHz
+  // MODE[1:0] = 11 // 50 MHz
+  // ----------------------------
+  // 
+  // speed - 50MHz
+  GPIOA->CRL |= GPIO_CRL_MODE2;
+
+  // loop for led blinking
+  while (1) {
+    // led ON
+    GPIOA->BSRR |= GPIO_BSRR_BS2;
+    // delay
+    wait();
+    // led OFF
+    GPIOA->BRR |= GPIO_BSRR_BR2;
+    // delay
+    wait();      
+  }
+}
+
+#ifdef USE_FULL_ASSERT
+  void assert_failed(uint8_t* file, uint32_t line)
+  {
+    // Use GDB to find out why we're here
+    while (1);
+  }
+#endif
